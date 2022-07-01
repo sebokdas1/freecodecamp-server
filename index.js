31b6d5610038f22d857e5f401c7c6f3b6e9603f7
sebokdas1 <sebok.das.ff@gmail.com>
2022-07-01 22:28:32 +0600

    jwt pass

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.kloii.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//verifying JWT
+function verifyJWT(req, res, next) {
+    console.log('abc')
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('freecodecamp').collection('users');
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        //get user information
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+            res.send({ result, token });
+        });
+    }
+    finally { }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello freecodecamp!');
+});
+
+app.listen(port, () => {
+    console.log(`codecamp app listening on port ${port}`);
+});
